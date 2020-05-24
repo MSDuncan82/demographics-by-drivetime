35bc469338b0221fad33738dceed0fedadbbc52a
Michael Duncan <msduncan82@gmail.com>
2020-05-24 16:10:12 -0600

    black formatted and working on load_sql

--- src/data/load_s3.py
+++ src/data/load_s3.py
@@ -4,19 +4,21 @@ import pandas as pd
 import os
 import uuid
 
-class S3Exec():
 
+class S3Exec:
     def __init__(self,):
         """Initiate boto connection to S3"""
 
-        self.s3_client = boto3.client('s3')
-        self.s3_resource = boto3.resource('s3')
+        self.paths = Paths()
+        self.census_data_path = self.paths.data.census_description.path
+        self.s3_client = boto3.client("s3")
+        self.s3_resource = boto3.resource("s3")
         self.session = boto3.session.Session()
 
     def create_bucket_name(self, bucket_prefix):
         """The generated bucket name must be between 3 and 63 chars long"""
 
-        return ''.join([bucket_prefix, str(uuid.uuid4())])
+        return "".join([bucket_prefix, str(uuid.uuid4())])
 
     def create_bucket(self, bucket_prefix, s3_connection):
         """Create bucket with unique name"""
@@ -29,35 +31,41 @@ class S3Exec():
 
         bucket_response = s3_connection.create_bucket(
             Bucket=bucket_name,
-            CreateBucketConfiguration={
-            'LocationConstraint': current_region})
+            CreateBucketConfiguration={"LocationConstraint": current_region},
+        )
 
         return bucket_name, bucket_response
-    
+
     def generate_random_filename(self, filename):
         """Generate filename with random prefix"""
 
-        return ''.join([str(uuid.uuid4().hex[:6]), f'_{filename}'])
+        return "".join([str(uuid.uuid4().hex[:6]), f"_{filename}"])
 
     def get_excel_file(self, url):
         """Get DataFrame from excel file"""
 
         df = pd.read_excel(url)
-        filename = url.split('/')[-1].split('.')[-2]
+        filename = url.split("/")[-1].split(".")[-2]
         return df, filename
 
-    def write_excel_file(self, df, filename, data_path=census_data_path):
+    def write_excel_file(self, df, filename, data_path=None):
         """Write excel file from DataFrame to csv"""
 
+        if data_path is None:
+            data_path = self.census_data_path
+
         random_file_name = self.generate_random_filename(filename)
-        filepath = f'{data_path}{random_file_name}.csv'
+        filepath = f"{data_path}{random_file_name}.csv"
         df.to_csv(filepath)
-        
+
     def rename_existing_files(self, filepath):
-        filename = filepath.split('/')[-1].split('.')[-2]
-        filedir = os.path.join(*filepath.split('/')[:-1])
+        """Remame existing file"""
+
+        filename = filepath.split("/")[-1].split(".")[-2]
+        filedir = os.path.join(*filepath.split("/")[:-1])
         random_file_name = self.generate_random_filename(filename)
-        os.rename(filepath, f'{filedir}/{random_file_name}.csv')
-    
-if __name__ == '__main__':
-    print(proj_path())
+        os.rename(filepath, f"{filedir}/{random_file_name}.csv")
+
+
+if __name__ == "__main__":
+    pass
--- src/data/load_sql.py
+++ src/data/load_sql.py
@@ -1,23 +1,41 @@
 from src.data.connect_db import create_engine_to_rds
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from geoalchemy2 import Geometry
 
-db = create_engine_to_rds('census_data')
 Base = declarative_base()
 
-table_list = pd.read_excel("https://www2.census.gov/programs-surveys/acs/tech_docs/table_shells/table_lists/2018_DataProductList.xlsx?#")
 
-class 
+class CensusTableList:
+    """
+    SqlAlchemy `table_data` Class
+    
+    Primary Key: table_code (i.e. "B01003")
+    """
 
-def excel_to_sql(path):
+    pass
+
+
+class SqlExec:
+    def __init__(self, db="census_data"):
+
+        self.db = create_engine_to_rds("census_data")
+
+    def excel_to_sql(self, path):
+        """Save excel table as sql table"""
+
+        table_list = pd.read_excel(
+            "https://www2.census.gov/programs-surveys/acs/tech_docs/table_shells/table_lists/2018_DataProductList.xlsx?#"
+        )
+
 
 class Test(Base):
-    __tablename__ = 'test'
+    __tablename__ = "test"
 
     col1 = Column(String, primary_key=True)
     col2 = Column(Integer)
+
 
 Session = sessionmaker(db)
 session = Session()
@@ -30,16 +48,19 @@ def create():
     session.add(row_1)
     session.commit()
 
+
 # Read
 def read():
     data = session.query(Test)
     for datum in data:
         print(datum.col1, datum.col2)
 
+
 # Update
 def update(row_1):
     row_1.col2 = 10
     session.commit()
+
 
 def delete(row_1):
     session.delete(row_1)
