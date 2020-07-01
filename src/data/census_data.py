3c5240fb4520a0cda098ce9f193820a9cc4b0b59
Michael Duncan <msduncan82@gmail.com>
2020-07-01 11:57:29 -0600

    start integrating census data

---
+++ src/data/census_data.py
@@ -0,0 +1,32 @@
+from api_wrapper.census_api.census_api import CensusDataAPI
+import os
+import shutil
+import uuid
+import pandas as pd
+import numpy as np
+
+from src.data.s3 import S3Exec
+from proj_paths.paths import Paths
+
+
+class CensusData(object):
+    def __init__(self, year=2018, survey="acs5"):
+
+        self.paths = Paths()
+        self.census_data = CensusDataAPI(survey="acs5", year=2018)
+
+    def get_county_data(self, state, county="*"):
+
+        kwargs = dict(state=state, county=county)
+        hierarchy = self.census_data._parse_hierarchy(kwargs)
+        df = self.census_data.get_data(state=state, county=county)
+        import ipdb
+
+        ipdb.set_trace()
+        return df
+
+
+if __name__ == "__main__":
+
+    census_data = CensusData()
+    df = census_data.get_county_data(state="Colorado")
