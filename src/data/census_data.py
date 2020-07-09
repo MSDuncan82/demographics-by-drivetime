3335480f77a9959cfde94c4cc8c3b31a6598dfad
Michael Duncan <msduncan82@gmail.com>
2020-07-09 06:33:39 -0600

    added documentation and refactored

--- src/data/census_data.py
+++ src/data/census_data.py
@@ -8,8 +8,26 @@ import numpy as np
 from src.data.s3 import S3Exec
 from proj_paths.paths import Paths
 
-
 class CensusData(object):
+    """
+    Retrieve data from census
+    
+    Access the CensusDataAPI from the api_wrapper package to retrieve census data.
+    
+    Attributes
+    ----------
+    census_data : CensusDataAPI
+        CensusDataAPI from api_wrapper. Takes `year` and `survey` in __init__.
+        Defaults are year=2018, survey="acs5". See api_wrapper docs for more 
+        details
+    
+    Methods
+    ---------
+    get_county_data(state, county="*")
+        Get default data from CensusDataAPI for a given state and county.
+        Default is to get data from all counties in state. TODO Add tables kwarg
+    """
+    
     def __init__(self, year=2018, survey="acs5"):
 
         self.paths = Paths()
@@ -18,13 +36,13 @@ class CensusData(object):
     def get_county_data(self, state, county="*"):
 
         kwargs = dict(state=state, county=county)
-        hierarchy = self.census_data._parse_hierarchy(kwargs)
+        # hierarchy = self.census_data._parse_hierarchy(kwargs)
         df = self.census_data.get_data(state=state, county=county)
 
         return df
-
 
 if __name__ == "__main__":
 
     census_data = CensusData()
     df = census_data.get_county_data(state="Colorado")
+    import ipdb; ipdb.set_trace()
