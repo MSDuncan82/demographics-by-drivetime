e55c5c071ac0416b0945eaa6a1a18cd57a0f00f5
Michael Duncan <msduncan82@gmail.com>
2020-07-09 08:22:59 -0600

    moving load_sql to a class

--- src/data/load_sql.py
+++ src/data/load_sql.py
@@ -7,78 +7,101 @@ import logging
 from datetime import datetime
 
 
-def createtable_table_metadata(sql_exec):
-    """
-    SqlAlchemy `table_metadata` Class
+class SqlLoader(object):
+    # TODO current finish moving functions to class methods
+    # TODO add data functionality
+    # TODO docs
+    def __init__(self, cmd_args):
 
-    Primary Key: table_id (i.e. "B01003_001")
+        self.cmd_args = cmd_args
+        self._check_args(cmd_args)
 
-    Source Files: 
-        https://www2.census.gov/programs-surveys/acs/summary_file/2018/data/2018_5yr_Summary_FileTemplates.zip?#
-    """
+        self.boundaries = cmd_args['boundaries']
+        self.demographic = cmd_args['boundaries']
 
-    table_metadata_df = CensusDescription().get_table_metadata_df()
-    table_metadata_cols = table_metadata_df.reset_index().columns
+    def _check_args(self, cmd_args):
+        """Check:
+        - If at least one of the arguments evaluates to True
+        - If one of `boundaries` or `data` is True but not both (XOR)"""
 
-    table_metadata = sql_exec.create_table_class(
-        table_metadata_cols, "table_metadata", "TableMetaData"
-    )
+        if not any([arg_value for arg_value in cmd_args.__dict__.values()]):
+            raise AttributeError("You need to specify at least one argument")
 
-    sql_exec.add(table_metadata_df, name="table_metadata", if_exists="replace")
+        if cmd_args['boundaries'] != cmd_args['data']: # XOR
+            raise AttributeError("You cannot load boundaries and data at the same time")
 
-    print("table_metadata created and filled")
+    def createtable_table_metadata(self, sql_exec, boundaries=True, demographic=False):
+        """
+        SqlAlchemy `table_metadata` Class
 
+        Primary Key: table_id (i.e. "B01003_001")
 
-def createtable_county_boundaries(sql_exec):
-    """
-    SqlAlchemy `county_boundaries` Class
-    
-    Primary Key: GEOID {State FIP}{County FIP} 
+        Source Files: 
+            https://www2.census.gov/programs-surveys/acs/summary_file/2018/data/2018_5yr_Summary_FileTemplates.zip?#
+        """
 
-    Source Files: 
-        https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
-    """
+        table_metadata_df = CensusDescription().get_table_metadata_df()
+        table_metadata_cols = table_metadata_df.reset_index().columns
 
-    county_geom_gdf = CensusGeometry().get_county_boundaries()
-    county_geom_df = pd.DataFrame(county_geom_gdf, dtype=str)
+        table_metadata = sql_exec.create_table_class(
+            table_metadata_cols, "table_metadata", "TableMetaData"
+        )
 
-    county_geom_cols = county_geom_df.reset_index().columns
+        sql_exec.add(table_metadata_df, name="table_metadata", if_exists="replace")
 
-    county_boundaries = sql_exec.create_table_class(
-        county_geom_cols, "county_boundaries", "CountyBoundaries"
-    )
-
-    sql_exec.add(county_geom_df, name="county_boundaries", if_exists="replace")
-
-    print("county_boundaries table created and filled")
-
-
-def createtable_block_boundaries(sql_exec, state):
-    """
-    SqlAlchemy `block_boundaries` Class
-    
-    Primary Key: GEOID {State FIP}{County FIP}{Tract FIP}{Block FIP} 
-
-    Source Files: 
-        https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
-    """
-
-    block_geom_gdf = CensusGeometry().get_block_boundaries(state)
-    block_geom_df = pd.DataFrame(block_geom_gdf, dtype=str)
-    del block_geom_gdf
-
-    block_geom_cols = block_geom_df.reset_index().columns
-
-    block_boundaries = sql_exec.create_table_class(
-        block_geom_cols, "block_boundaries", "BlockBoundaries"
-    )
-
-    sql_exec.add(block_geom_df, name="block_boundaries", if_exists="append")
-
-    print(f"block_boundaries table created and filled with {state} blocks")
+        print("table_metadata created and filled")
 
 
-def setup_argparse(boundaries=True):
+    def createtable_county_boundaries(self, sql_exec, boundaries=True, demographic=False):
+        """
+        SqlAlchemy `county_boundaries` Class
+        
+        Primary Key: GEOID {State FIP}{County FIP} 
+
+        Source Files: 
+            https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
+        """
+
+        county_geom_gdf = CensusGeometry().get_county_boundaries()
+        county_geom_df = pd.DataFrame(county_geom_gdf, dtype=str)
+
+        county_geom_cols = county_geom_df.reset_index().columns
+
+        county_boundaries = sql_exec.create_table_class(
+            county_geom_cols, "county_boundaries", "CountyBoundaries"
+        )
+
+        sql_exec.add(county_geom_df, name="county_boundaries", if_exists="replace")
+
+        print("county_boundaries table created and filled")
+
+
+    def createtable_block_boundaries(self, sql_exec, state, boundaries=True, demographic=False):
+        """
+        SqlAlchemy `block_boundaries` Class
+        
+        Primary Key: GEOID {State FIP}{County FIP}{Tract FIP}{Block FIP} 
+
+        Source Files: 
+            https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
+        """
+
+        block_geom_gdf = CensusGeometry().get_block_boundaries(state)
+        block_geom_df = pd.DataFrame(block_geom_gdf, dtype=str)
+        del block_geom_gdf
+
+        block_geom_cols = block_geom_df.reset_index().columns
+
+        block_boundaries = sql_exec.create_table_class(
+            block_geom_cols, "block_boundaries", "BlockBoundaries"
+        )
+
+        sql_exec.add(block_geom_df, name="block_boundaries", if_exists="append")
+
+        print(f"block_boundaries table created and filled with {state} blocks")
+
+
+def setup_argparse(boundaries=True)
     """
     Parse arguments from command line for Makefile.
 
@@ -95,43 +118,39 @@ def setup_argparse(boundaries=True):
 
     parser = argparse.ArgumentParser("Choose what to load into database")
 
-    if boundaries:
-        parser.add_argument(
-            "-m", "--meta", help="load metadata into sql db", action="store_true"
-        )
-        parser.add_argument(
-            "-c",
-            "--county",
-            help="load county boundaries into sql db",
-            action="store_true",
-        )
-        parser.add_argument(
-            "-s",
-            "--state",
-            help="select the state of the block boundaries you want to load",
-            type=str,
-        )
-        parser.add_argument(
-            "-l",
-            "--log",
-            help="select the level of logging [debug, info, none]",
-            type=str,
-            default="none",
-        )
-        parser._get_args()
+    parser.add_argument(
+        "-b", "--boundaries", help="load census boundary data", action="store_true"
+    )
+    parser.add_argument(
+        "-d", "--data", help="load census demographic data", action="store_true"
+    )
+    parser.add_argument(
+        "-m", "--meta", help="load metadata into sql db", action="store_true"
+    )
+    parser.add_argument(
+        "-c",
+        "--county",
+        help="load county demographic data/boundaries into sql db",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-s",
+        "--state",
+        help="select the state of the data you want to load",
+        type=str,
+    )
+    parser.add_argument(
+        "-l",
+        "--log",
+        help="select the level of logging [debug, info, none]",
+        type=str,
+        default="none",
+    )
 
+    parser._get_args()
     cmd_args = parser.parse_args()
-    check_args(cmd_args)
 
     return cmd_args
-
-
-def check_args(cmd_args):
-    """Check if at least one of the arguments evaluates to True"""
-
-    if not any([arg_value for arg_value in cmd_args.__dict__.values()]):
-        raise AttributeError("You need to specify at least one argument")
-
 
 def main_wrapper(main):
     def inner_wrapper():
@@ -160,7 +179,7 @@ def main(cmd_args=None, sql_exec=None):
     Main function of load_sql that is to be used at the command line or in
     the Makefile to load the sql database with census data.
     """
-
+    # TODO use class instead
     if cmd_args.meta:
         createtable_table_metadata(sql_exec)
 
